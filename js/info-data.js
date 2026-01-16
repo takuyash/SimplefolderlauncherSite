adea9ac41b42b03d35561c28877cd3f0b5670fb2
DESKTOP-G7I2LER\takuya <14888534+takuyash@users.noreply.github.com>
2026-01-16 23:00:43 +0900

    リリース情報の追加。説明の修正。軽微な修正。

--- js/info-data.js
+++ js/info-data.js
@@ -1,8 +1,17 @@
 var infos = [
   {
+    date: "2026-01-16",
+    title: "v0.6.0 をリリースしました",
+    body: "・Shift 2回連続押下でも起動するように修正<br>・パスが設定されていない時にランチャー画面にメッセージを表示するように修正",
+    link: {
+      url: "https://github.com/takuyash/SimpleFolderLauncher/releases",
+      label: "GitHub Releases を見る"
+    }
+  },
+  {
     date: "2026-01-12",
     title: "v0.5.0 をリリースしました",
-    body: "設定画面でフォルダダイアログから設定できるように修正,ヘルプサイトの修正",
+    body: "・設定画面でフォルダダイアログから設定できるように修正<br>・ヘルプサイトの修正",
     link: {
       url: "https://github.com/takuyash/SimpleFolderLauncher/releases",
       label: "GitHub Releases を見る"
